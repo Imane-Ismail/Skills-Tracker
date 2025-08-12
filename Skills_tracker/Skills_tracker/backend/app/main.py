d78fc12c63c262d0cfc9278bc829fafbaf7ce06f
Imane Ismail <86661348+Imane-Ismail@users.noreply.github.com>
2025-08-13 01:11:14 +1000

    Update main.py

--- Skills_tracker/Skills_tracker/backend/app/main.py
+++ Skills_tracker/Skills_tracker/backend/app/main.py
@@ -3,6 +3,10 @@ from app.routes import users, teams, skills, skill_status, requests
 
 app = FastAPI(title="MDR Skills Tracker")
 
+@app.get("/")
+def root():
+    return {"message": "Welcome to the MDR Skills Tracker API!"}
+  
 app.include_router(users.router)
 app.include_router(teams.router)
 app.include_router(skills.router)
